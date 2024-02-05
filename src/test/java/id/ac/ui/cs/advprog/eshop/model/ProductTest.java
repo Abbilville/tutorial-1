a5869e1280803e08ef7fe051cbab985ea70fa08b
Abbilville <abbilville@gmail.com>
2024-02-05 22:57:52 +0700

    fix: fix typo

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -11,7 +11,7 @@ public class ProductTest {
     void setUp() {
         this.product = new Product();
         this.product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        this.product.setProductName("Sampo Cam Bambang");
+        this.product.setProductName("Sampo Cap Bambang");
         this.product.setProductQuantity(100);
     }
 
