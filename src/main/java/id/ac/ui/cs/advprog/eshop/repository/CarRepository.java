6e8513f2187d3c18fce5b7b3f69a2f2ce7da098e
Abbilville <abbilville@gmail.com>
2024-02-24 08:47:28 +0700

    Enhancing for loop

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -35,8 +35,7 @@ public class CarRepository {
     }
 
     public Car update(String id, Car updatedCar) {
-        for (int i = 0; i < carData.size(); i++) {
-            Car car = carData.get(i);
+        for (Car car : carData) {
             if (car.getCarId().equals(id)) {
                 // Update the existing car with the new information
                 car.setCarName(updatedCar.getCarName());
