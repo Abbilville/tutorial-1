ea12f54ef3c789b8433357782cbc73d8fa2aa4db
Abbilville <abbilville@gmail.com>
2024-02-24 08:41:50 +0700

    Single Responsibility Principle: Remove extends Product Controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/car")
-class CarController extends ProductController {
+class CarController {
     @Autowired
     private CarServiceImpl carService;
 
