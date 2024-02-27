0086a6fb94bf8a1d1ccb0b5fa262ca14bc09cbf8
Abbilville <abbilville@gmail.com>
2024-02-27 07:24:05 +0700

    remove unused import

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,7 +1,5 @@
 package id.ac.ui.cs.advprog.eshop;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
