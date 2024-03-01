4d43d332e5222720622a073cb7af57559b151e27
Abbilville <abbilville@gmail.com>
2024-03-01 10:28:52 +0700

    [RED] Add tests for OrderRepository (fix typo)

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -32,14 +32,14 @@ public class OrderRepositoryTest {
         products.add(product1);
 
         orders = new ArrayList<>();
-        Order order1 = new Order("13652556-012a-407-b546-54eb1396d79b",
-            products, 1708560000L, "Safina Sudrajat");
+        Order order1 = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+            products, 1708560000L, "Safira Sudrajat");
         orders.add(order1);
         Order order2 = new Order("7f9e15bb-4b15-42f4-aebc-c3af385fb078",
             products, 1708570000L, "Safira Sudrajat");
         orders.add(order2);
-        Order order3 = new Order("e334ef40-9eff-4da8-9487-8ee697ecbfle",
-            products, 1708570080L, "Bambang Sudrajat");
+        Order order3 = new Order("e334ef40-9eff-4da8-9487-8ee697ecbf1e",
+            products, 1708570000L, "Bambang Sudrajat");
         orders.add(order3);
     }
 
