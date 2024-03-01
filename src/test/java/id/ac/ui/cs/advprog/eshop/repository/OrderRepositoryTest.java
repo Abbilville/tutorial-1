c5eec8f91d6f4a08e2532d2a8950e218b9e7551d
Abbilville <abbilville@gmail.com>
2024-03-01 10:31:41 +0700

    [REFACTOR] Move enums folder to src/main/java/id/ac/ui/cs/advprog/eshop

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import id.ac.ui.cs.advprog.eshop.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
