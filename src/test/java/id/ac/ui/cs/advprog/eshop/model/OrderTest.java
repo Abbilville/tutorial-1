cb33d9af11cad25c87d93cc29bec3c494b1c50e1
Abbilville <abbilville@gmail.com>
2024-03-06 18:46:16 +0700

    [REFACTOR] add Supress Warning

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -32,6 +32,7 @@ public class OrderTest {
         this.products.clear();
 
         assertThrows(IllegalArgumentException.class, () -> {
+            @SuppressWarnings("unused")
             Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                 this.products, 1708560000L, "Safira Sudrajat");
         });
@@ -64,6 +65,7 @@ public class OrderTest {
     @Test
     void testCreateOrderInvalidStatus() {
         assertThrows(IllegalArgumentException.class, () -> {
+            @SuppressWarnings("unused")
             Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                 this.products, 1708560000L, "Safira Sudrajat", "MEOW");
         });
