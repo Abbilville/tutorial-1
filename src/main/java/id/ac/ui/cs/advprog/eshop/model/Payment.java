f5f17a64390bf625b5659037813dfae91e87ee5a
Abbilville <abbilville@gmail.com>
2024-03-06 17:18:51 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,10 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import java.util.Map;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Builder
 @Getter
@@ -13,12 +14,43 @@ public class Payment {
     String method;
     Order order;
     Map<String, String> paymentData;
-    @Setter
     String status;
 
-    public Payment(String id, String method, Order order, Map<String, String> paymentData) {
+    public Payment(String id, String method, Order order, Map<String, String> paymentData, String status) {
+        this.id = id;
+        this.method = method;
+        this.setOrder(order);
+        this.setPaymentData(paymentData);
+        this.setStatus(status);
     }
 
-    public Payment(String id, String method, Order order, Map<String, String> paymentData, String status) {
+    public Payment(String id, String method, Order order, Map<String, String> paymentData) {
+        this(id, method, order, paymentData, PaymentStatus.PENDING.getValue());
+    }
+
+    public void setStatus(String status) {
+        if (!PaymentStatus.contains(status)) {
+            throw new IllegalArgumentException("Invalid payment status");
+        }
+
+        this.status = status;
+    }
+
+    public void setOrder(Order order) {
+        if (order == null) {
+            throw new IllegalArgumentException("Order cannot be null");
+        }
+
+        this.order = order;
+    }
+
+    protected void setPaymentData(Map<String, String> paymentData) {
+        if (PaymentMethod.contains(this.method)) {
+            throw new IllegalArgumentException(
+                "Cannot set method-specific payment data for non-method-specific payment"
+            );
+        }
+
+        this.paymentData = null;
     }
 }
